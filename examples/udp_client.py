c5ef2532f6e02aa82a9b0444e490c61a4530ce97
legohead259 <legohead259@gmail.com>
2022-10-11 14:20:43 +0100

    Experimentation

--- examples/udp_client.py
+++ examples/udp_client.py
@@ -1,14 +1,15 @@
 import socket
+import struct
 
 msgFromClient = "Hello UDP Server"
 bytesToSend = str.encode(msgFromClient)
-serverAddressPort = ("0.0.0.0", 20001)
+serverAddressPort = ("0.0.0.0", 42069)
 bufferSize = 1024
 
-UDPClientSocket = socket.socket(family=AF_INET, type=socket.SOCK_DGRAM)
+UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 
-UDPClientSocket.sendTo(bytesToSend, serverAddressPort)
+UDPClientSocket.sendto(bytesToSend, serverAddressPort)
 
 msgFromServer = UDPClientSocket.recvfrom(bufferSize)
 
-print("Message from server: ", msgFromServer)
+print("Message from server: ", struct.unpack('f', msgFromServer[0]))
