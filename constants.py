22847c602844492ea7af7cbfe3a07cf2ec72c05a
Braidan <legohead259@gmail.com>
2022-11-20 09:20:49 -0700

    Modified MAV util master to use new topside computer IP address

--- constants.py
+++ constants.py
@@ -18,7 +18,7 @@ from pymavlink import mavutil
 global master
 print("Establishing connection to MAVlink server...")
 # TODO: Get local client IP address and put into the mavlink connection function
-master = mavutil.mavlink_connection('udpin:192.168.2.6:14550') # Opens a mavlink connection at the local server
+master = mavutil.mavlink_connection('udpin:192.168.2.1:14550') # Opens a mavlink connection at the local server
 
 import time
 global boot_time 
